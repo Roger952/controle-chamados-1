1780165d7addf133c45ed357f8e55c4f35c5caef
gustavo.bruder <gugabruder@gmail.com>
2020-01-03 13:59:02 -0300

    Cadastro de Atendente incompleto.

--- src/main/java/br/com/hbsis/controlechamados/usuario/atendente/AtendenteRest.java
+++ src/main/java/br/com/hbsis/controlechamados/usuario/atendente/AtendenteRest.java
@@ -1,4 +1,33 @@
 package br.com.hbsis.controlechamados.usuario.atendente;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/atendente")
 public class AtendenteRest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AtendenteRest.class);
+    private final AtendenteService  atendenteService;
+
+    @Autowired /** CONSTRUTOR */
+    public AtendenteRest(AtendenteService atendenteService) {
+        this.atendenteService = atendenteService;
+    }
+
+    /** MÉTODOS */
+    @PostMapping
+    public AtendenteDTO save(@RequestParam("file") MultipartFile file, @RequestBody AtendenteDTO atendenteDTO){
+
+        LOGGER.info("Recebendo save para atendente...");
+        LOGGER.debug("Payload: {}", atendenteDTO);
+
+        //files.add(file.getOriginalFilename());
+        return this.atendenteService.save(file, atendenteDTO);
+    }
 }
--- src/main/java/br/com/hbsis/controlechamados/usuario/atendente/AtendenteService.java
+++ src/main/java/br/com/hbsis/controlechamados/usuario/atendente/AtendenteService.java
@@ -1,4 +1,47 @@
 package br.com.hbsis.controlechamados.usuario.atendente;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
 public class AtendenteService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AtendenteService.class);
+    private final IAtendenteRepository iAtendenteRepository;
+
+    private final Path rootLocation = Paths.get("img");
+
+    @Autowired /** CONSTRUTOR */
+    public AtendenteService(IAtendenteRepository iAtendenteRepository) {
+        this.iAtendenteRepository = iAtendenteRepository;
+    }
+
+    /** MÉTODOS DE CRUD */
+    public AtendenteDTO save(MultipartFile file, AtendenteDTO atendenteDTO) {
+
+        LOGGER.info("Salvando atendente");
+        LOGGER.debug("Atendente: {}", atendenteDTO);
+
+        Atendente atendente = new Atendente();
+        atendente.setNome(atendenteDTO.getNome());
+        atendente.setEmail(atendenteDTO.getEmail());
+        atendente.setSenha(atendenteDTO.getSenha());
+
+        try {
+            Files.copy(file.getInputStream(), this.rootLocation.resolve(atendenteDTO.getFoto() + ".png"));
+            atendente.setFoto(atendenteDTO.getFoto());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Erro no upload de imagem");
+        }
+
+        atendente = this.iAtendenteRepository.save(atendente);
+        return atendenteDTO.of(atendente);
+    }
 }
