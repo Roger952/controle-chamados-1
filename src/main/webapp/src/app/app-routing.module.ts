8d804ad758713a659514f9fa51467338c9920983
LucaBLobe <lucablobe@gmail.com>
2020-01-06 10:31:12 -0300

    Empresa Frontend: html cadastrar.

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginAdminComponent } from './login-admin/login-admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProdutosComponent } from './produtos/produtos.component';
+import { EmpresaComponent } from './empresa/empresa.component';
 
 
 const routes: Routes = [
   { path: 'login-admin', component: LoginAdminComponent, },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'produtos', component: ProdutosComponent },
+  { path: 'empresa', component: EmpresaComponent },
   { path: '', redirectTo: '/login-admin', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
