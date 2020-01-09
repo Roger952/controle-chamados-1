2c0f663a132a7e7fb377f58be6339d1d324dfe0b
LucaBLobe <lucablobe@gmail.com>
2020-01-09 16:50:46 -0300

    Cadastro Módulo front-end

---
+++ src/main/webapp/src/app/create-modulo/create-modulo.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateModuloComponent } from './create-modulo.component';
+
+describe('CreateModuloComponent', () => {
+  let component: CreateModuloComponent;
+  let fixture: ComponentFixture<CreateModuloComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreateModuloComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateModuloComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
