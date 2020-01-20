34b9ca609e084f32a68e35f609698c9e23ce333e
GustavoBabarone <gugabruder@gmail.com>
2020-01-20 15:30:27 -0300

    BUGFIX - Modelagem banco de dados

--- src/main/resources/db/migration/V20200103105933__seg_admin.sql
+++ src/main/resources/db/migration/V20200103105933__seg_admin.sql
@@ -1,7 +1,7 @@
-create table seg_admin(
-    id BIGINT IDENTITY (1,1) primary key,
-    login VARCHAR(100) NOT NULL,
-    senha VARCHAR(100)  NOT NULL
-)
+ CREATE TABLE seg_admin(
+    id             BIGINT IDENTITY(1, 1) NOT NULL,
+    login                   VARCHAR(100) NOT NULL,
+    senha                   VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_id_admin PRIMARY KEY (id)
+);
 
-create unique index ix_seg_admin_login on seg_admin(id);
