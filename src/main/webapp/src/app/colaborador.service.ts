6ebfd50dd03626cedd971aec2c7c55862d6fc328
GustavoBabarone <gugabruder@gmail.com>
2020-01-23 15:20:23 -0300

    BUGFIX - Front colaborador

--- src/main/webapp/src/app/colaborador.service.ts
+++ src/main/webapp/src/app/colaborador.service.ts
@@ -15,17 +15,7 @@ export class ColaboradorService {
 
   constructor(private http: ControleHttp) { }
 
-  // getAtendente(id: number): Observable<any> {
-  //   return this.http.get(`${this.baseUrl+'/findById'}/${id}`);
-  // }
-
   createColaborador(colaborador: Colaborador): Observable<Object> {
-
-    console.log("Colaborador: "+ colaborador.email);
-    console.log("Colaborador: "+ colaborador.nome);
-    console.log("Colaborador: "+ colaborador.senha);
-    console.log("Colaborador: "+ colaborador.empresaId);
-    console.log("Colaborador: "+ colaborador.produtoList);
 
     const headers = new HttpHeaders().append('Authorization', 'Bearer' + localStorage.getItem('token'));
     return this.http.post(`${this.baseUrl+'/save'}`, colaborador, { headers });
@@ -38,18 +28,6 @@ export class ColaboradorService {
    deleteColaborador(id: number): Observable<any> {
         return this.http.delete(`${this.baseUrl+'/delete'}/${id}`, { responseType: 'text' });
    }
-
-  /* FILE-UPLOAD */
-//   uploadImg(file: File): Observable<any>{
-
-//     console.log("Arquivo (file) dentro do método uploadimg: "+file.name);
-
-//     let url = this.baseUrl + '/saveImagem';
-//     let formData: FormData = new FormData();
-//     formData.append('file', file);
-//     const headers = new HttpHeaders().append('Authorization', 'Bearer' + localStorage.getItem('token'));
-//     return this.http.post(url, formData, { headers });
-//   }
 
   getColaboradorList(): Observable<Colaborador[]> {
     const headers = new HttpHeaders().append('Authorization', 'Bearer' + localStorage.getItem('token'));
